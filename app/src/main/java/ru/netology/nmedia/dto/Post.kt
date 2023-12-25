c17c95b0f6297d9f9b004e0b96aa24db3cf99501
Viktor-89 <89821787772@ya.ru>
2023-12-25 18:21:03 +0500

    5 mvvm

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -8,31 +8,31 @@ data class Post (
     val author: String,
     val published: String, //дата публикации
     val content: String,
-    var LikeByMe: Boolean = false,//был ли проставлен лайк
-    var likes: Int = 0,//кол-во лайков, значение по умолчанию 0
-    var repost: Boolean =true,//был ли проставлен репост
-    var repostCount: Int =999, //количество репостов
+    val LikeByMe: Boolean = false,//был ли проставлен лайк
+    val likes: Int = 0,//кол-во лайков, значение по умолчанию 0
+    val repost: Boolean =true,//был ли проставлен репост
+    val repostCount: Int =999, //количество репостов
 
 )
-fun countFormat(likesCount: Int): String {
-    return when (likesCount) {
-        in 1000..1099 ->"${roundNoDecimal(likesCount.toDouble()/1_000.0)}K"
-        in 1100..9_999 ->"${roundWithDecimal(likesCount.toDouble()/1_000.0)}K"
-        in 10_000..999_999 ->"${roundNoDecimal(likesCount.toDouble()/1_000.0)}K"
-        in 1_000_000..1_099_999 ->"${roundNoDecimal(likesCount.toDouble()/1_000_000.0)}M"
-        in 1_100_000..Int.MAX_VALUE ->"${roundWithDecimal(likesCount.toDouble()/1_000_000.0)}M"
-
-        else-> likesCount.toString()
-    }
-}
-
-fun roundWithDecimal(number: Double): Double? {
-    val df = DecimalFormat("#.#")
-    df.roundingMode = RoundingMode.FLOOR
-    return df.format(number).toDouble()
-}
-fun roundNoDecimal(number: Double): Int? {
-    val df = DecimalFormat("#")
-    df.roundingMode = RoundingMode.FLOOR
-    return df.format(number).toInt()
-}
+//fun countFormat(likesCount: Int): String {
+//    return when (likesCount) {
+//        in 1000..1099 ->"${roundNoDecimal(likesCount.toDouble()/1_000.0)}K"
+//        in 1100..9_999 ->"${roundWithDecimal(likesCount.toDouble()/1_000.0)}K"
+//        in 10_000..999_999 ->"${roundNoDecimal(likesCount.toDouble()/1_000.0)}K"
+//        in 1_000_000..1_099_999 ->"${roundNoDecimal(likesCount.toDouble()/1_000_000.0)}M"
+//        in 1_100_000..Int.MAX_VALUE ->"${roundWithDecimal(likesCount.toDouble()/1_000_000.0)}M"
+//
+//        else-> likesCount.toString()
+//    }
+//}
+//
+//fun roundWithDecimal(number: Double): Double? {
+//    val df = DecimalFormat("#.#")
+//    df.roundingMode = RoundingMode.FLOOR
+//    return df.format(number).toDouble()
+//}
+//fun roundNoDecimal(number: Double): Int? {
+//    val df = DecimalFormat("#")
+//    df.roundingMode = RoundingMode.FLOOR
+//    return df.format(number).toInt()
+//}
