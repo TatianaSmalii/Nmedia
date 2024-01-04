d81745e1c47d656b97623ce781bfa0d3ddce567f
Viktor-89 <89821787772@ya.ru>
2024-01-05 03:14:28 +0500

    6 recyclerview (add scroll on posts and posts)

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -1,108 +1,76 @@
 package ru.netology.nmedia.activity
 
-//import ru.netology.nmedia.dto.countFormat
-//import ru.netology.nmedia.dto.roundNoDecimal
-//import ru.netology.nmedia.dto.roundWithDecimal
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.databinding.CardPostBinding
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        println("onCreate $this")
+        //println("onCreate $this")
         //работаем с Binding в gradle в нашем проекте
-        val binding = ActivityMainBinding.inflate(layoutInflater) //получаем Binding и вызываем на нем метод inflate, layoutInflater - свойство активити
+        val binding =
+            ActivityMainBinding.inflate(layoutInflater) //получаем Binding и вызываем на нем метод inflate, layoutInflater - свойство активити
         setContentView(binding.root) //передаю метод sed cont-view
 
-        val viewModel by viewModels <PostViewModel> ()
-        viewModel.data.observe(this){post ->
-            with(binding) {
-                autor.text = post.author
-                published.text = post.published
-                content.text = post.content
-                likeCount.text = reduceNumber(post.likes)
-                repostCount.text = reduceNumber(post.repostCount)
-                like.setImageResource(if (post.LikeByMe) R.drawable.baseline_favorite_24 else R.drawable.baseline_favorite_border_24)
-                //likeCount.text = post.likes.toString()
-                repost.setImageResource(if (post.repost) R.drawable.baseline_share_24 else R.drawable.baseline_share_black_24)
-                //repostCount.text = post.repostCount.toString()
-                //avatar.setImageResource(R.drawable.ic_launcher_foreground)
+        //val viewModel by viewModels <PostViewModel> ()
+        val viewModel: PostViewModel by viewModels()
+        val adapter = PostsAdapter(
+            { viewModel.likeById(it.id) },
+            { viewModel.repost(it.id) }
+        )
+        binding.list.adapter = adapter
+        viewModel.data.observe(this) { posts ->
+            adapter.submitList(posts)
+
+//            binding.container.removeAllViews()//очистка постов перед заполнением и далее их добавляем
+//            posts.map { post ->
+//                CardPostBinding.inflate(layoutInflater, binding.container,true).apply{
+//                    autor.text = post.author
+//                    published.text = post.published
+//                    content.text = post.content
+//                    likeCount.text = reduceNumber(post.likes)
+//                    like.setImageResource(
+//                        if (post.LikeByMe) R.drawable.baseline_favorite_24 else R.drawable.baseline_favorite_border_24
+//                    )
+//                    like.setOnClickListener {
+//                        viewModel.likeById(post.id)
+//                    }
+//                    repostCount.text = reduceNumber(post.repostCount)
+//                    repost.setImageResource(
+//                        if (post.repost) R.drawable.baseline_share_24 else R.drawable.baseline_share_black_24
+//                    )
+//                    repost.setOnClickListener{
+//                        viewModel.repost(post.id)
+//                    }
+//                }.root
+//            }
+//            with(binding) {
+//                likeCount.text = reduceNumber(post.likes)
+//                repostCount.text = reduceNumber(post.repostCount)
+//                like.setImageResource(if (post.LikeByMe) R.drawable.baseline_favorite_24 else R.drawable.baseline_favorite_border_24)
+//                repost.setImageResource(if (post.repost) R.drawable.baseline_share_24 else R.drawable.baseline_share_black_24)
+            //avatar.setImageResource(R.drawable.ic_launcher_foreground)
 
 //                binding.root.setOnClickListener {//для обработчика
 //                    Log.d("root", "обработчик на root сработал")
 //                }
-            }
-        }
-        binding.like.setOnClickListener {
-            viewModel.like()
-        }
-
-        binding.repost.setOnClickListener{
-            viewModel.repost()
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        println("onDestroy $this")
-    }
-//    fun countFormat(likesCount: Int): String {
-//        return when (likesCount) {
-//            in 1000..1099 ->"${roundNoDecimal(likesCount.toDouble()/1_000.0)}K"
-//            in 1100..9_999 ->"${roundWithDecimal(likesCount.toDouble()/1_000.0)}K"
-//            in 10_000..999_999 ->"${roundNoDecimal(likesCount.toDouble()/1_000.0)}K"
-//            in 1_000_000..1_099_999 ->"${roundNoDecimal(likesCount.toDouble()/1_000_000.0)}M"
-//            in 1_100_000..Int.MAX_VALUE ->"${roundWithDecimal(likesCount.toDouble()/1_000_000.0)}M"
-//
-//            else-> likesCount.toString()
-//        }
-//    }
-//    fun roundWithDecimal(number: Double): Double? {
-//        val df = DecimalFormat("#.#")
-//        df.roundingMode = RoundingMode.FLOOR
-//        return df.format(number).toDouble()
-//    }
-//    fun roundNoDecimal(number: Double): Int? {
-//        val df = DecimalFormat("#")
-//        df.roundingMode = RoundingMode.FLOOR
-//        return df.format(number).toInt()
-//    }
-fun reduceNumber(count: Int): String {
-    val oneThousands = count / 1000
-    val hundredThousands = (count % 1_000_000) / 100_000
-    val oneHundreds = (count % 1000) / 100
-    val oneMillion = (count / 1_000_000)
-
-    when {
-        count < 1000 -> return "$count"
-        count in 1000..9_999 -> {
-
-            return if (count % 1000 in 0..99) {
-                "${oneThousands}k"
-            } else {
-                return "${oneThousands}.${oneHundreds}k"
-            }
-        }
-
-        count in 10_000..999_999 -> {
-            return "${oneThousands}k"
-        }
-
-        else -> {
-            return if (count % 1_000_000 in 0..99_999) {
-                "${oneMillion}M"
-            } else {
-                "${oneMillion}.${hundredThousands}M"
-            }
+//            }
         }
     }
 }
-}
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        println("onDestroy $this")
+//    }
+//}
+
 
 
 
--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -1,8 +1,5 @@
 package ru.netology.nmedia.dto
 
-import java.math.RoundingMode
-import java.text.DecimalFormat
-
 data class Post (
     val id: Long,
     val author: String,
@@ -11,28 +8,5 @@ data class Post (
     val LikeByMe: Boolean = false,//был ли проставлен лайк
     val likes: Int = 0,//кол-во лайков, значение по умолчанию 0
     val repost: Boolean =true,//был ли проставлен репост
-    val repostCount: Int =999, //количество репостов
-
+    val repostCount: Int =0, //количество репостов
 )
-//fun countFormat(likesCount: Int): String {
-//    return when (likesCount) {
-//        in 1000..1099 ->"${roundNoDecimal(likesCount.toDouble()/1_000.0)}K"
-//        in 1100..9_999 ->"${roundWithDecimal(likesCount.toDouble()/1_000.0)}K"
-//        in 10_000..999_999 ->"${roundNoDecimal(likesCount.toDouble()/1_000.0)}K"
-//        in 1_000_000..1_099_999 ->"${roundNoDecimal(likesCount.toDouble()/1_000_000.0)}M"
-//        in 1_100_000..Int.MAX_VALUE ->"${roundWithDecimal(likesCount.toDouble()/1_000_000.0)}M"
-//
-//        else-> likesCount.toString()
-//    }
-//}
-//
-//fun roundWithDecimal(number: Double): Double? {
-//    val df = DecimalFormat("#.#")
-//    df.roundingMode = RoundingMode.FLOOR
-//    return df.format(number).toDouble()
-//}
-//fun roundNoDecimal(number: Double): Int? {
-//    val df = DecimalFormat("#")
-//    df.roundingMode = RoundingMode.FLOOR
-//    return df.format(number).toInt()
-//}
