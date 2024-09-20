0b49644ca18c8d7378d635911292822f2f81284d
Viktor-89 <89821787772@ya.ru>
2024-09-20 19:36:59 +0500

    loading avatars (Home Work 6)

---
+++ app/src/main/java/nmedia/handler/Image.kt
@@ -0,0 +1,26 @@
+package ru.netology.nmedia.handler
+
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import ru.netology.nmedia.R
+import okhttp3.HttpUrl
+import okio.Timeout
+
+fun ImageView.loadAvatars(url: String, timeout: Int = 30_000){
+    Glide.with(this)
+        .load(url)
+        .timeout(timeout)
+        .placeholder(R.drawable.ic_loading_100dp)
+        .error(R.drawable.ic_error_100dp)
+        .circleCrop()
+        .into(this)
+}
+
+fun ImageView.loadAttachment(url: String, timeout: Int = 30_000){
+    Glide.with(this)
+        .load(url)
+        .timeout(timeout)
+        .placeholder(R.drawable.ic_loading_100dp)
+        .error(R.drawable.ic_error_100dp)
+        .into(this)
+}
